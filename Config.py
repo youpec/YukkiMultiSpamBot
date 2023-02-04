22b5902e430502e5c445dac640a73a546215cfdb
youpec <124452082+youpec@users.noreply.github.com>
2023-02-04 21:46:57 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-STRING = getenv("STRING")
+STRING = getenv("AgGH9LUAXsp3l0jOx7QnVmnTwBbcEv__bJFNjEJflPN9BHRP4qJd-fE4WhnBPn10tTbwbkhRaT0G9pGZ17rt0W5O6Yg1Lk65K0FPrSbY0r9BWXH9gEEOk6T6oTjVsH8veyY6Awxa126R1BqJ5snRyEfag608wT3R-FISgWU4WG134XTSl6KQvKxIc05_kFzhhZ1_P7NT8BGwC853Gew9ttsADde6IoAK8KzDlYTbSaz1qkd4by8QiRq0MvYRuJKunecC1JZeqnopHiF7tWgNhoa_mpOCJl0Ji6SNIC25bP-IqEtXNmX8IQyxKkWKz49JJ4CBv0wzv9XGv3X9Ud8Y1qrWNyMn0QAAAAFaMSW5AA")
 STRING2 = getenv("STRING2")
 STRING3 = getenv("STRING3")
 STRING4 = getenv("STRING4")
@@ -13,7 +13,7 @@ STRING7 = getenv("STRING7")
 STRING8 = getenv("STRING8")
 STRING9 = getenv("STRING9")
 STRING10 = getenv("STRING10")
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
+API_ID = getenv("25687221")
+API_HASH = getenv("62bce18516592bf3d3003b329a0c8910")
 BIO_MESSAGE = getenv("BIO")
 SUDO = list(map(int, getenv("SUDO").split()))
